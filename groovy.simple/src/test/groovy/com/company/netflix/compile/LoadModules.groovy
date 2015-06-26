0f71be21a62a6b62f8c90690e800a7832dd97ec3
Eugene Kalinin <kalinin.eugene@gmail.com>
2015-06-26 13:17:37 +0300

    Workable version with compilers (BytecodeLoadingPlugin & Groovy2CompilerPlugin)

--- groovy.simple/src/test/groovy/com/company/netflix/compile/LoadModules.groovy
+++ groovy.simple/src/test/groovy/com/company/netflix/compile/LoadModules.groovy
@@ -31,6 +31,12 @@ class LoadModules extends Specification{
 
     private static final String GROOVY2_COMPILER_PLUGIN = Groovy2CompilerPlugin.class.getName();
 
+    def 'paths'(){
+        setup:
+        Path path = Paths.get('src/test/resources/libs/modules2/repository/spock-core.jar');
+        println "path:${path} :" + path.toAbsolutePath()
+    }
+
     def 'load'(){
         setup:
         ScriptModuleLoader moduleLoader = createGroovyModuleLoader().build();
@@ -39,25 +45,33 @@ class LoadModules extends Specification{
                 .addCompilerPluginId(BytecodeLoadingPlugin.PLUGIN_ID)
                 .addCompilerPluginId(Groovy2CompilerPlugin.PLUGIN_ID)
                 .build()
-        ScriptArchive scriptArchive = new JarScriptArchive.Builder(Paths.get('/opt/github/reversemind/anticoating/groovy.simple/src/test/resources/libs/modules2/repository/spock-core.jar'))
+        ScriptArchive scriptArchive = new JarScriptArchive.Builder(Paths.get('src/test/resources/libs/modules2/repository/spock-core.jar').toAbsolutePath())
                 .setCreateTime(new Date().getTime())
                 .setModuleSpec(_moduleSpec)
                 .build();
-        moduleLoader.updateScriptArchives(new LinkedHashSet<ScriptArchive>(Arrays.asList(scriptArchive)));
+//        moduleLoader.updateScriptArchives(new LinkedHashSet<ScriptArchive>(Arrays.asList(scriptArchive)));
 
 
 
-        ScriptModuleSpec moduleSpec = new ScriptModuleSpec.Builder(ModuleId.create("p90test-module"))
-//                .addModuleDependency("p90jvm-common")
-//                .addModuleDependency("p90jvm-component")
-//                .addModuleDependency("p90sdk-api")
-                .addModuleDependency("spock-core")
+        _moduleSpec = new ScriptModuleSpec.Builder(ModuleId.create("nicobar-core"))
 //                .addCompilerPluginId(BytecodeLoadingPlugin.PLUGIN_ID)
-                .addCompilerPluginId(Groovy2CompilerPlugin.PLUGIN_ID)
+//                .addCompilerPluginId(Groovy2CompilerPlugin.PLUGIN_ID)
+                .build()
+        scriptArchive = new JarScriptArchive.Builder(Paths.get('src/test/resources/libs/modules2/repository/nicobar-core.jar').toAbsolutePath())
+                .setCreateTime(new Date().getTime())
+                .setModuleSpec(_moduleSpec)
+                .build();
+//        moduleLoader.updateScriptArchives(new LinkedHashSet<ScriptArchive>(Arrays.asList(scriptArchive)));
 
+
+        ScriptModuleSpec moduleSpec = new ScriptModuleSpec.Builder(ModuleId.create("simple-module"))
+//                .addModuleDependency("spock-core")
+//                .addModuleDependency("nicobar-core")
+                .addCompilerPluginId(BytecodeLoadingPlugin.PLUGIN_ID)
+                .addCompilerPluginId(Groovy2CompilerPlugin.PLUGIN_ID)
                 .build();
 
-        def scriptRootPath = Paths.get('/opt/github/reversemind/anticoating/groovy.simple/src/test/resources/libs/modules2/script')
+        def scriptRootPath = Paths.get('src/test/resources/libs/modules2/script').toAbsolutePath()
         scriptArchive = new PathScriptArchive.Builder(scriptRootPath)
                 .setRecurseRoot(true)
                 .setModuleSpec(moduleSpec)
@@ -68,18 +82,17 @@ class LoadModules extends Specification{
 
 
 
-
-
-
-
-
-
-        ScriptModule scriptModule = moduleLoader.getScriptModule("p90test-module")
+        ScriptModule scriptModule = moduleLoader.getScriptModule("simple-module")
 
         println "size:" + scriptModule.getModuleClassLoader().getLoadedClasses().size()
 
+
+        println "!!!" + scriptModule.getLoadedClasses()
+
         Class<?> clazz = ScriptModuleUtils.findClass(scriptModule, "go")
 //        Class<?> clazz = findClassByName(scriptModule, "go");
+
+        ((clazz.newInstance()).getMetaClass()).
 
         println "go!"
         Script script = InvokerHelper.createScript(clazz, new Binding());
@@ -109,17 +122,48 @@ class LoadModules extends Specification{
         ScriptCompilerPluginSpec pluginSpec = new ScriptCompilerPluginSpec.Builder(Groovy2Compiler.GROOVY2_COMPILER_ID)
                 .addRuntimeResource(ExampleResourceLocator.getGroovyRuntime())
                 .addRuntimeResource(ExampleResourceLocator.getGroovyPluginLocation())
-        .addRuntimeResource(Paths.get("/opt/github/reversemind/anticoating/groovy.simple/src/test/resources/libs/modules2/lib"))
+                .addRuntimeResource(getByteCodeLoadingPluginLocation())
+        .addRuntimeResource(Paths.get("src/test/resources/libs/modules2/lib").toAbsolutePath())
 
         // hack to make the gradle build work. still doesn't seem to properly instrument the code
         // should probably add a classloader dependency on the system classloader instead
                 .addRuntimeResource(getCoberturaJar(getClass().getClassLoader()))
-
-                .withPluginClassName(BytecodeLoadingPlugin.class.getName())
                 .withPluginClassName(Groovy2CompilerPlugin.class.getName())
                 .build();
 
+
+        ScriptCompilerPluginSpec pluginSpecByteCode = new ScriptCompilerPluginSpec.Builder(BytecodeLoadingPlugin.PLUGIN_ID)
+                .addRuntimeResource(ExampleResourceLocator.getGroovyRuntime())
+                .addRuntimeResource(ExampleResourceLocator.getGroovyPluginLocation())
+                .addRuntimeResource(getByteCodeLoadingPluginLocation())
+
+                // TODO need make addRuntimeResourceS - add jar's from path - like a shared libraries
+                .addRuntimeResource(Paths.get("src/test/resources/libs/modules2/lib").toAbsolutePath())
+                .addRuntimeResource(Paths.get("src/test/resources/libs/modules2/lib/junit-4.12.jar").toAbsolutePath())
+                .addRuntimeResource(Paths.get("src/test/resources/libs/modules2/lib/nicobar-core.jar").toAbsolutePath())
+                .addRuntimeResource(Paths.get("src/test/resources/libs/modules2/lib/nicobar-groovy2-0.2.7-SNAPSHOT.jar").toAbsolutePath())
+                .addRuntimeResource(Paths.get("src/test/resources/libs/modules2/lib/spock-core-0.7-groovy-2.0.jar").toAbsolutePath())
+                .addRuntimeResource(Paths.get("src/test/resources/libs/modules2/lib/joda-time-2.8.1.jar").toAbsolutePath())
+                .addRuntimeResource(getCoberturaJar(getClass().getClassLoader()))
+                .withPluginClassName(BytecodeLoadingPlugin.class.getName())
+                .build();
+
+
+
         // create and start the builder with the plugin
-        return new ScriptModuleLoader.Builder().addPluginSpec(pluginSpec);
+        return new ScriptModuleLoader.Builder()
+                .addPluginSpec(pluginSpec)
+                .addPluginSpec(pluginSpecByteCode);
+
+    }
+
+
+    public static Path getByteCodeLoadingPluginLocation() {
+        String resourceName = ClassPathUtils.classNameToResourceName("com.netflix.nicobar.core.plugin.BytecodeLoadingPlugin");
+        Path path = ClassPathUtils.findRootPathForResource(resourceName, ExampleResourceLocator.class.getClassLoader());
+        if (path == null) {
+            throw new IllegalStateException("coudln't find BytecodeLoadingPlugin plugin jar in the classpath.");
+        }
+        return path;
     }
 }
