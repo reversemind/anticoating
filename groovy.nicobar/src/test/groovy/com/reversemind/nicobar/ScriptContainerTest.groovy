5cd4d445b5d9a6af2200f755475655cf9def7f27
Eugene Kalinin <kalinin.eugene@gmail.com>
2015-06-28 04:57:53 +0300

    Added Script Container + module compiler

--- groovy.nicobar/src/test/groovy/com/reversemind/nicobar/ScriptContainerTest.groovy
+++ groovy.nicobar/src/test/groovy/com/reversemind/nicobar/ScriptContainerTest.groovy
@@ -3,6 +3,8 @@ package com.reversemind.nicobar
 import com.netflix.nicobar.core.archive.JarScriptArchive
 import com.netflix.nicobar.core.archive.ModuleId
 import com.netflix.nicobar.core.archive.ScriptArchive
+import com.netflix.nicobar.groovy2.internal.compile.Groovy2CompilerHelper
+import org.codehaus.groovy.tools.GroovyClass
 import spock.lang.Specification
 
 import java.nio.file.Paths
@@ -18,6 +20,11 @@ class ScriptContainerTest extends Specification {
         ScriptArchive scriptArchive = new JarScriptArchive.Builder(Paths.get('src/test/resources/libs/sublevel.jar').toAbsolutePath())
 
                 .build();
+
+        Set<GroovyClass> compiledClasses = new Groovy2CompilerHelper(Paths.get('/opt/CompileGroovy2'))
+                .addScriptArchive(scriptArchive)
+                .compile();
+
 
         ScriptContainer scriptContainer = ScriptContainer.getInstance()
         scriptContainer.updateScriptArchive(scriptArchive)
