0062a731644837f779e1703a6ffa862d11db0861
Eugene Kalinin <kalinin.eugene@gmail.com>
2015-07-12 18:29:03 +0300

    Added file hash (sha1) + as git calculation

---
+++ groovy.nicobar/src/test/groovy/com/reversemind/nicobar/module/CreateModuleFromDirectoryTest.groovy
@@ -0,0 +1,45 @@
+package com.reversemind.nicobar.module
+
+import com.google.common.hash.HashCode
+import com.google.common.hash.HashFunction
+import com.google.common.hash.Hasher
+import com.google.common.hash.Hashing
+import groovy.util.logging.Slf4j
+import spock.lang.Specification
+
+/**
+ *
+ */
+@Slf4j
+class CreateModuleFromDirectoryTest extends Specification {
+
+    def 'create module form directory'() {
+        setup:
+        log.info "GO"
+
+
+        // #1 Watch directory
+
+        // #2 Create module for directory
+
+        // #3
+
+
+        // Guava sha1 - git file hash
+        HashFunction hashFunction = Hashing.sha1();
+        Hasher hasher = hashFunction.newHasher()
+        hasher.putBytes("".getBytes("UTF-8"))
+
+        HashCode hashCode = hasher.hash()
+        println "sha1:" + hashCode.toString()
+
+
+        // http://stackoverflow.com/questions/7225313/how-does-git-compute-file-hashes
+
+        /*
+        How does GIT compute its commit hashes
+
+        Commit Hash (SHA1) = SHA1("blob" + <size_of_file> + "\0" + <contents_of_file>)
+         */
+    }
+}
