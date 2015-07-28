36d76f307e111a6f8787953053362df5e146b9a7
Eugene Kalinin <kalinin.eugene@gmail.com>
2015-07-28 18:49:03 +0300

    Normalize names

--- groovy.nicobar/src/main/groovy/com/reversemind/nicobar/ScriptContainerConfig.java
+++ groovy.nicobar/src/main/groovy/com/reversemind/nicobar/ScriptContainerConfig.java
@@ -1,10 +1,14 @@
 package com.reversemind.nicobar;
 
 import java.io.Serializable;
+import java.nio.file.Path;
+import java.util.Set;
 
 /**
  *
  */
 public class ScriptContainerConfig implements Serializable {
+
+    private Set<Path> runTimeLibPath;
 
 }
