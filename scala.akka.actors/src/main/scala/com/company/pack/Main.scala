e048f817a0a46592fea47fe82a32960288dcb339
Eugene Kalinin <kalinin.eugene@gmail.com>
2015-10-16 16:50:58 +0300

    Explore Scala + Spray / RestApi Actor

--- scala.akka.actors/src/main/scala/com/company/pack/Main.scala
+++ scala.akka.actors/src/main/scala/com/company/pack/Main.scala
@@ -1,14 +1,17 @@
 package com.company.pack
 
 import akka.actor.{ActorSystem, Props}
-import com.typesafe.config.ConfigFactory
+import akka.util.Timeout
+import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.scalalogging.LazyLogging
+import spray.can.Http
 
 import scala.concurrent.duration.{Duration, FiniteDuration}
 
 /**
  *
  */
-object Main extends App {
+object Main extends App with RequestTimeout with ShutdownIfNotBound with LazyLogging {
 
   implicit val system = ActorSystem("ScalaActorExplorer")
   implicit val executionContext = system.dispatcher
@@ -17,8 +20,40 @@ object Main extends App {
   val host = config.getString("http.host")
   val port = config.getInt("http.port")
 
-  val _t = config.getString("spray.can.server.request-timeout")
+  val _t = requestTimeout(config)
   implicit val requestTimeout = FiniteDuration(Duration(_t).length, Duration(_t).unit)
 
   val api = system.actorOf(Props(new RestApi(requestTimeout)), "httpInterface")
+}
+
+trait RequestTimeout {
+
+  import scala.concurrent.duration._
+
+  def requestTimeout(config: Config): Timeout = {
+    val t = config.getString("spray.can.server.request-timeout")
+    val d = Duration(t)
+    FiniteDuration(d.length, d.unit)
+  }
+}
+
+trait ShutdownIfNotBound {
+
+  import scala.concurrent.{ExecutionContext, Future}
+
+  def shutdownIfNotBound(f: Future[Any])(implicit system: ActorSystem, ec: ExecutionContext) = {
+    f.mapTo[Http.Event].map {
+      case Http.Bound(address) =>
+        println(s"REST interface bound to $address")
+
+      case Http.CommandFailed(cmd) =>
+        println(s"REST interface could not bind: ${cmd.failureMessage}, shutting down.")
+        system.shutdown()
+
+    }.recover {
+      case e: Throwable =>
+        println(s"Unexpected error binding to HTTP: ${e.getMessage}, shutting down.")
+        system.shutdown()
+    }
+  }
 }
