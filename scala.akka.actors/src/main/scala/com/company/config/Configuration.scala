9abc5314774cfd4c994a76860cc8f4b20b415083
Eugene Kalinin <kalinin.eugene@gmail.com>
2015-10-19 10:34:50 +0300

    Extract Configuration into separate trait

---
+++ scala.akka.actors/src/main/scala/com/company/config/Configuration.scala
@@ -0,0 +1,32 @@
+package com.company.config
+
+import akka.util.Timeout
+import com.typesafe.config.{Config, ConfigFactory}
+
+import scala.util.Try
+
+/**
+ *
+ */
+trait Configuration extends RequestTimeout{
+
+  val config = ConfigFactory.load()
+  lazy val host = Try(config.getString("http.host")).getOrElse("127.0.0.1")
+  lazy val port = Try(config.getInt("http.port")).getOrElse(8080)
+
+  lazy implicit val requestChunkAggregationLimit = config.getString("spray.can.server.request-chunk-aggregation-limit").toLong
+
+  lazy implicit val timeout = requestTimeout(config)
+
+}
+
+trait RequestTimeout {
+
+  import scala.concurrent.duration._
+
+  def requestTimeout(config: Config): Timeout = {
+    val t = config.getString("spray.can.server.request-timeout")
+    val d = Duration(t)
+    FiniteDuration(d.length, d.unit)
+  }
+}
--- scala.akka.actors/src/main/scala/com/company/pack/Main.scala
+++ scala.akka.actors/src/main/scala/com/company/pack/Main.scala
@@ -2,63 +2,41 @@ package com.company.pack
 
 import akka.actor.{ActorSystem, Props}
 import akka.io.IO
-import akka.util.Timeout
 import akka.pattern.ask
-import com.typesafe.config.{Config, ConfigFactory}
 import com.typesafe.scalalogging.LazyLogging
 import spray.can.Http
-
-import scala.concurrent.duration.{Duration, FiniteDuration}
-import scala.util.Try
 
 /**
  *
  */
-object Main extends App with RequestTimeout with ShutdownIfNotBound{
+object Main extends App with ShutdownIfNotBound with com.company.config.Configuration {
 
   implicit val actorSystem = ActorSystem("ScalaActorExplorer")
   implicit val executionContext = actorSystem.dispatcher
 
-  val config = ConfigFactory.load()
-  val host = Try(config.getString("http.host")).getOrElse("127.0.0.1")
-  val port = Try(config.getInt("http.port")).getOrElse(8080)
-
-  implicit val requestChunkAggregationLimit = config.getString("spray.can.server.request-chunk-aggregation-limit").toLong
-
-  implicit val timeout = requestTimeout(config)
   val apiService = actorSystem.actorOf(Props(new RestApiActor(timeout)), "httpInterface")
 
   val response = IO(Http).ask(Http.Bind(listener = apiService, interface = host, port = port))
+
   shutdownIfNotBound(response)
 }
 
-trait RequestTimeout {
-
-  import scala.concurrent.duration._
-
-  def requestTimeout(config: Config): Timeout = {
-    val t = config.getString("spray.can.server.request-timeout")
-    val d = Duration(t)
-    FiniteDuration(d.length, d.unit)
-  }
-}
-
-trait ShutdownIfNotBound {
+trait ShutdownIfNotBound extends LazyLogging {
 
   import scala.concurrent.{ExecutionContext, Future}
 
   def shutdownIfNotBound(f: Future[Any])(implicit system: ActorSystem, ec: ExecutionContext) = {
     f.mapTo[Http.Event].map {
       case Http.Bound(address) =>
-        println(s"REST interface bound to $address")
+        logger.info(s"REST interface bound to $address")
 
       case Http.CommandFailed(cmd) =>
-        println(s"REST interface could not bind: ${cmd.failureMessage}, shutting down.")
+        logger.info(s"REST interface could not bind: ${cmd.failureMessage}, shutting down.")
         system.shutdown()
 
     }.recover {
       case e: Throwable =>
-        println(s"Unexpected error binding to HTTP: ${e.getMessage}, shutting down.")
+        logger.info(s"Unexpected error binding to HTTP: ${e.getMessage}, shutting down.")
         system.shutdown()
     }
   }
