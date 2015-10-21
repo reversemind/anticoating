cca3c7f93f292e9e10ad610b8babec94b1d6b1f9
Eugene Kalinin <kalinin.eugene@gmail.com>
2015-10-21 13:59:38 +0300

    Clean up / Scala + Akka + Camel + RabbitMQ + Feature

--- scala.akka.actors/src/main/scala/com/company/camel/Main.scala
+++ scala.akka.actors/src/main/scala/com/company/camel/Main.scala
@@ -1,8 +1,11 @@
 package com.company.camel
 
+import java.util.Date
+
 import akka.actor.Actor.Receive
 import akka.actor.{ActorLogging, Status, ActorSystem, Props}
 import akka.camel.{Ack, CamelMessage, Consumer, Oneway}
+import akka.io.Tcp.Register
 import akka.util.Timeout
 import com.fasterxml.jackson.core.JsonParseException
 import com.typesafe.scalalogging.LazyLogging
@@ -20,25 +23,68 @@ object Main extends App with LazyLogging{
 
   implicit val timeout = Timeout(6 seconds)
 
-  implicit val log = logger
-
   val endPoint = "rabbitmq://localhost:5672/exchangePrefetch?queue=prefetch.queue&autoAck=false&autoDelete=false&automaticRecoveryEnabled=true&exchangeType=topic&routingKey=routingKey"
 
   val producer = actorSystem.actorOf(Props(new SimpleProducer(endPoint)), name = "simpleProducer")
   val consumer = actorSystem.actorOf(Props(new SimpleConsumer(endPoint)), name = "simpleConsumer")
 
-  log.info("Push message")
+  logger.info("Push message")
   producer ! "fake message:0"
 
-  for(i <-1 to 100){
+  for(i <-1 to 10){
     producer ! s"fake message:$i"
     Thread.sleep(1000)
   }
 
 }
 
-class SimpleProducer(_endpointUri: String) extends Oneway {
+class SimpleProducer(_endpointUri: String) extends Oneway with LazyLogging{
   override def endpointUri: String = _endpointUri
+
+  override def preStart() {
+    super.preStart()
+    logger.info(s"SimpleProducer pre start at:${new Date()}")
+
+    Thread.sleep(2000)
+    logger.info(s"MessageProducer pre ended at:${new Date()}")
+
+    val _paths = context.actorSelection("akka://ActorAfterFeature/user/*")
+    logger.info(s"actor selection:${_paths}")
+  }
+
+  override def transformOutgoingMessage(message: Any): Any = {
+    message match {
+      case msg: CamelMessage => {
+        try {
+          val content = msg.bodyAs[String]
+          logger.info(s"Produce a message:$content")
+
+        } catch {
+          case ex: Exception =>
+            "TransformException: %s".format(ex.getMessage)
+        }
+        msg
+      }
+      case other => message
+    }
+  }
+
+  override def transformResponse(message: Any): Any = {
+    message match {
+      case msg: CamelMessage => {
+        try {
+          val content = msg.bodyAs[String]
+          logger.info(s"Produce a message:$content")
+          msg
+        } catch {
+          case ex: Exception =>
+            "TransformException: %s".format(ex.getMessage)
+        }
+      }
+      case other => message
+    }
+  }
+
 }
 
 class SimpleConsumer(_endpointUri: String) extends Consumer with ActorLogging{
