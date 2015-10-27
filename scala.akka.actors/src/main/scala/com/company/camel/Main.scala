07469a4cd6cd5a1a803107b94910688037527b00
Eugene Kalinin <kalinin.eugene@gmail.com>
2015-10-27 17:48:36 +0300

    Temp solution / Added PostActor to emulate external service fail / Scala + Akka + Camel + RabbitMQ + Feature

--- scala.akka.actors/src/main/scala/com/company/camel/Main.scala
+++ scala.akka.actors/src/main/scala/com/company/camel/Main.scala
@@ -2,13 +2,13 @@ package com.company.camel
 
 import akka.actor._
 import akka.camel._
-import akka.pattern.{ask, pipe}
+import akka.pattern.ask
 import akka.util.Timeout
-
+import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
 
+import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.concurrent.{Future, TimeoutException}
 import scala.language.postfixOps
 import scala.util.{Failure, Random, Success}
 
@@ -17,7 +17,13 @@ import scala.util.{Failure, Random, Success}
  */
 object Main extends App with LazyLogging with Configuration {
 
-  implicit val actorSystem = ActorSystem(actorSystemName)
+  implicit val actorSystem = ActorSystem(actorSystemName, ConfigFactory.parseString("""
+                                                                                      akka {
+                                                                                        actor {
+                                                                                          timeout = 20
+                                                                                        }
+                                                                                      }
+                                                                                    """))
   implicit val contextExecutor = actorSystem.dispatcher
   val camel = CamelExtension(actorSystem)
 
@@ -28,7 +34,7 @@ object Main extends App with LazyLogging with Configuration {
 
 
   // get a future reference to the activation of the endpoint of the Consumer Actor
-//  val activationFuture = camel.activationFutureFor(consumerActor)(timeout = 20 seconds, executor = contextExecutor)
+  //  val activationFuture = camel.activationFutureFor(consumerActor)(timeout = 20 seconds, executor = contextExecutor)
 
   logger.info("Push messages")
 
@@ -48,15 +54,37 @@ class SimpleConsumer(_endpointUri: String) extends Consumer with ActorLogging {
   import context.dispatcher
 
   val MAX_RETRIES = 3
-  val RETRY_DELAY = 5 seconds
+  val RETRY_DELAY = 2 seconds
 
   override def autoAck = false
+
   implicit val timeout = Timeout(20 seconds)
+
   override def endpointUri: String = _endpointUri
+
   var counter: Long = 0L
+
+  val postActor = context.actorOf(Props(PostActor))
 
   override def receive = {
     case msg: CamelMessage => {
+
+      // # stage 2
+      //
+      //      val message = msg.bodyAs[String]
+      //      log.info(s"Income message:$message")
+      //      sender() ! Status.Failure(new RuntimeException("RuntimeException"))
+      //
+      //
+      //      counter += 1
+      //
+      //      if(counter == 2){
+      //        context.stop(self)
+      //      }
+
+
+      // # stage 1
+
       val _sender = sender()
       val _self = self
 
@@ -64,28 +92,57 @@ class SimpleConsumer(_endpointUri: String) extends Consumer with ActorLogging {
 
       counter += 1
 
-      val postActor = context.actorOf(Props(PostActor))
-
       val delayed = akka.pattern.after(RETRY_DELAY, using = context.system.scheduler)(
-        Future(_sender ! Status.Failure(new RuntimeException("============================RuntimeException")))
+        Future(_sender ! Status.Failure(new RuntimeException("RuntimeException")))
       )
 
-      val future = Future firstCompletedOf Seq(postActor ? message, delayed)
+      lazy val future = Future firstCompletedOf Seq(postActor ? message, delayed)
 
-      future onComplete {
-        case Success(notification) =>
-          log.info(s"!!! Success !!! = Message is POSTed:$notification")
-          _sender ! Ack
+      future.onComplete {
+        case Success(notification) => notification match {
+
+            case "NOT_OK" => {
+              log.info(s"!!! IS NOT Success !!! = Message is NOT POSTed:'$notification'")
+              _sender ! Status.Failure(new RuntimeException("RuntimeException #3"))
+            }
+
+            case _ => {
+              log.info(s"!!! Success !!! = Message is POSTed:'$notification'")
+              _sender ! Ack
+            }
+
+          }
+
+          //          context.stop(postActor)
         case Failure(ex) =>
           log.error(s"!!! Failure !!! = Message is NOT POSTed", ex)
+          //          context.stop(postActor)
           _sender ! Status.Failure(ex)
       }
 
-//      future pipeTo sender()
+
+      //      future onSuccess {
+      //        case notification: String => {
+      //          log.info(s"!!! Success !!! = Message is POSTed:'$notification'")
+      //          context.stop(postActor)
+      //          _sender ! Ack
+      //        }
+      //      }
+      //
+      //      future onFailure  {
+      //        case ex: RuntimeException => {
+      //            log.error(s"!!! Failure !!! = Message is NOT POSTed", ex)
+      //            context.stop(postActor)
+      ////            _sender ! Status.Failure(ex)
+      //        }
+      //      }
+
+      //      sender() ! Status.Failure(new RuntimeException("RuntimeException #2"))
+      ////      future pipeTo sender()
     }
-    case other =>
-      log.info(s"Message:$other")
-      sender() ! Ack
+    //    case other =>
+    //      log.info(s"Message:$other")
+    //      sender() ! Ack
   }
 }
 
@@ -97,21 +154,31 @@ object PostActor extends Actor with LazyLogging {
     case message: String => {
 
       counter += 1
-      val sleepFor = new Random().nextInt(1 * 1000)
+      val sleepFor = new Random().nextInt(1 * 3000)
       logger.info(s"\nCounter is:$counter and will sleep for:$sleepFor ms with message:'$message'\n")
 
-      //      Thread.sleep(sleepFor)
+//      Thread.sleep(sleepFor)
 
-      if(message.equals("fake message:3")){
-        logger.info(s"\nUnable to send a POST for message:'$message' let's try again for POST counter:$counter\n")
-        sender() ! Status.Failure(new RuntimeException(s"Exception - unable to send a POST for $message"))
-      }else if (counter % 2 == 0) {
-        logger.info(s"\nUnable to send a POST for message:'$message' let's try again for POST counter:$counter\n")
-        sender() ! Status.Failure(new RuntimeException("Exception - unable to send a POST"))
-      } else {
-        logger.info(s"\nPOST was successfully sent for message:'$message' - for POST counter:$counter\n")
-        sender() ! s"\nPOST was successfully sent for message:'$message'\n"
+      //      if(message.equals("fake message:3")){
+      //        logger.info(s"\nUnable to send a POST for message:'$message' let's try again for POST counter:$counter\n")
+      //        sender() ! Status.Failure(new RuntimeException(s"Exception - unable to send a POST for $message"))
+      //      }else if (counter % 2 == 0) {
+      //        logger.info(s"\nUnable to send a POST for message:'$message' let's try again for POST counter:$counter\n")
+      //        sender() ! Status.Failure(new RuntimeException("Exception - unable to send a POST"))
+      //      } else {
+      //        logger.info(s"\nPOST was successfully sent for message:'$message' - for POST counter:$counter\n")
+      //        sender() ! s"\nPOST was successfully sent for message:'$message'\n"
+      //      }
+      if (message != null) {
+        if (message.equals("fake message:3")) {
+          logger.info(s"\nUnable to send a POST for message:'$message' let's try again for POST counter:$counter\n")
+          sender() ! "NOT_OK"; //Status.Failure(new IllegalArgumentException(s"IllegalArgumentException - unable to send a POST for $message"))
+        } else {
+          logger.info(s"\nPOST was successfully sent for message:'$message' - for POST counter:$counter\n")
+          sender() ! s"\nPOST was successfully sent for message:'$message'\n"
+        }
       }
+
     }
 
   }
