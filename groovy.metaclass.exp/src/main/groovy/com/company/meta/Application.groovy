4903d97b0d14f03694827bcb18c595e60f78ca4b
Eugene Kalinin <kalinin.eugene@gmail.com>
2016-04-16 15:08:17 +0300

    Experiment with groovy meta class

---
+++ groovy.metaclass.exp/src/main/groovy/com/company/meta/Application.groovy
@@ -0,0 +1,19 @@
+package com.company.meta
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ */
+class Application {
+
+    Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+
+    public static final
+
+    static void main(String... args) {
+//        log.info "go"
+    }
+
+}
---
+++ groovy.metaclass.exp/src/main/groovy/com/company/meta/inter/InterfaceSimple.java
@@ -0,0 +1,8 @@
+package com.company.meta.inter;
+
+/**
+ *
+ */
+public interface InterfaceSimple {
+    String getValue(String number, String code);
+}
